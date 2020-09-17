0c4ad3f972ea493d33f296224c8628225b1c666d
Bodo Graumann <mail@bodograumann.de>
2020-09-17 10:01:32 +0200

    Add additional simple tests

--- test_iconv.py
+++ test_iconv.py
@@ -1,4 +1,13 @@
 import iconv
+
+s=iconv.open("utf-8","latin1")
+r=s.iconv(b"Hallo")
+print(repr(r),len(r))
+
+s=iconv.open("utf-8","utf-16")
+r=s.iconv(b"Hallo", 10)
+print(repr(r),len(r))
+
 s=iconv.open("unicodelittle","iso-8859-1")
 r=s.iconv(b"Hallo",11,return_unicode=1)
 print(repr(r),len(r))
