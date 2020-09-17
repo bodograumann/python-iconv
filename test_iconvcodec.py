cd2410e4d920f06d8dd09b77c433cb771fb029ae
Bodo Graumann <mail@bodograumann.de>
2020-09-17 12:35:35 +0200

    Add test for transliteration

--- test_iconvcodec.py
+++ test_iconvcodec.py
@@ -10,3 +10,8 @@ class TestIconvcodecModule(unittest.TestCase):
     def test_decode(self):
         string = b"Hallo".decode("T.61")
         self.assertEqual(string, "Hallo")
+
+    def test_transliterate(self):
+        string = "abc ß α € àḃç"
+        bytestring = string.encode("ASCII//TRANSLIT")
+        self.assertEqual(bytestring, b"abc ss ? EUR abc")
