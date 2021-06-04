6ba069d524e81a3a3b1bc935552318e01972c5ff
Nate Woods <nwoods@redviolet.com>
2021-06-04 14:25:21 -0700

    Fix character indexing issues and type confusion. When buffers are too small, make them bigger.

--- iconvcodec.py
+++ iconvcodec.py
@@ -4,17 +4,19 @@ def codec_factory(encoding):
     encoder = iconv.open(encoding, "utf-8")
     decoder = iconv.open("utf-8", encoding)
 
-    def encode(input, errors="strict"):
+    def encode(input, errors="strict", bufsize=None):
         msg = input.encode()
+
+        if bufsize is None:
+            bufsize = len(msg)
+
         try:
-            return encoder.iconv(msg), len(msg)
+            return encoder.iconv(msg, bufsize), len(msg)
         except iconv.error as e:
             errstring, code, inlen, outres = e.args
-            assert inlen % 2 == 0
-            inlen /= 2
             if code == errno.E2BIG:
-                # outbuffer was too small, try to encode rest
-                out1, len1 = encode(msg[inlen:], errors)
+                # outbuffer was too small, increase size a bit and try to encode rest
+                out1, len1 = encode(msg[inlen :].decode(), errors, bufsize - inlen + max(bufsize // 10, 3))
                 return outres + out1, inlen + len1
             if code == errno.EINVAL:
                 # An incomplete multibyte sequence has been
@@ -27,22 +29,25 @@ def codec_factory(encoding):
                 if errors == "strict":
                     raise UnicodeError(*e.args)
                 if errors == "replace":
-                    out1, len1 = encode(u"?" + msg[inlen + 1 :], errors)
+                    out1, len1 = encode((u"?" + msg[inlen :].decode()[1:]), errors)
                 elif errors == "ignore":
-                    out1, len1 = encode(msg[inlen + 1 :], errors)
+                    out1, len1 = encode(msg[inlen :].decode()[1:], errors)
                 else:
                     raise ValueError("unsupported error handling")
-                return outres + out1, inlen + 1 + len1
+                return outres + out1, inlen + len1 + 1
             raise
 
-    def decode(msg, errors="strict"):
+    def decode(msg, errors="strict", bufsize=None):
+        if bufsize is None:
+            bufsize = len(msg)
+
         try:
-            return decoder.iconv(msg).decode(), len(msg)
+            return decoder.iconv(msg, bufsize).decode(), len(msg)
         except iconv.error as e:
             errstring, code, inlen, outres = e.args
             if code == errno.E2BIG:
                 # buffer too small
-                out1, len1 = decode(msg[inlen:], errors)
+                out1, len1 = decode(msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3))
                 return outres.decode() + out1, inlen + len1
             if code == errno.EINVAL:
                 # An incomplete multibyte sequence has been
@@ -55,13 +60,13 @@ def codec_factory(encoding):
                 if errors == "strict":
                     raise UnicodeError(*e.args)
                 if errors == "replace":
-                    outres += u"\uFFFD"
-                    out1, len1 = decode(msg[inlen:], errors)
+                    outres += u"\uFFFD".encode()
+                    out1, len1 = decode(msg[inlen + 1:], errors)
                 elif errors == "ignore":
-                    out1, len1 = decode(msg[inlen:], errors)
+                    out1, len1 = decode(msg[inlen + 1:], errors)
                 else:
                     raise ValueError("unsupported error handling")
-                return outres.decode() + out1, inlen + len1
+                return outres.decode() + out1, inlen + len1 + 1
 
     return encode, decode
 
