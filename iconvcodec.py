987f5eba2a7a84a4dfc823dd88092dc7a542c0db
Nate Woods <nwoods@redviolet.com>
2021-06-04 15:17:22 -0700

    Factor encode and decode implementations into separate functions

--- iconvcodec.py
+++ iconvcodec.py
@@ -1,72 +1,82 @@
 import sys, iconv, codecs, errno
 
+
+_ENCODE_REPLACECHAR = '?'.encode()
+_DECODE_REPLACECHAR = u'\uFFFD'.encode()
+
+def _iconv_encode_impl(encoder, msg, errors, bufsize=None):
+    if bufsize is None:
+        bufsize = len(msg)
+
+    try:
+        return encoder.iconv(msg, bufsize), len(msg)
+    except iconv.error as e:
+        errstring, code, inlen, outres = e.args
+        if code == errno.E2BIG:
+            # outbuffer was too small, increase size a bit and try to encode rest
+            out1, len1 = _iconv_encode_impl(encoder, msg[inlen :], errors, bufsize - inlen + max(bufsize // 10, 3))
+            return outres + out1, inlen + len1
+        if code == errno.EINVAL:
+            # An incomplete multibyte sequence has been
+            # encountered in the input. Should not happen in Unicode
+            raise AssertionError("EINVAL in encode")
+        if code == errno.EILSEQ:
+            # An invalid multibyte sequence has been encountered
+            # in the input. Used to indicate that the character is
+            # not supported in the target code
+            if errors == "strict":
+                raise UnicodeError(*e.args)
+            if errors == "replace":
+                out1, len1 = _iconv_encode_impl(encoder, (_ENCODE_REPLACECHAR + msg[inlen :].decode()[1:].encode()), errors)
+            elif errors == "ignore":
+                out1, len1 = _iconv_encode_impl(encoder, msg[inlen :].decode()[1:].encode(), errors)
+            else:
+                raise ValueError("unsupported error handling")
+            return outres + out1, inlen + len1 + 1
+        raise
+
+def _iconv_decode_impl(decoder, msg, errors, bufsize=None):
+    if bufsize is None:
+        bufsize = len(msg)
+
+    try:
+        return decoder.iconv(msg, bufsize).decode(), len(msg)
+    except iconv.error as e:
+        errstring, code, inlen, outres = e.args
+        if code == errno.E2BIG:
+            # buffer too small
+            out1, len1 = _iconv_decode_impl(decoder, msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3))
+            return outres.decode() + out1, inlen + len1
+        if code == errno.EINVAL:
+            # An incomplete multibyte sequence has been
+            # encountered in the input.
+            return outres.decode(), inlen
+        if code == errno.EILSEQ:
+            # An invalid multibyte sequence has been encountered
+            # in the input. Ignoring or replacing it is hard to
+            # achieve, just try one character at a time
+            if errors == "strict":
+                raise UnicodeError(*e.args)
+            if errors == "replace":
+                outres += _DECODE_REPLACECHAR
+                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1:], errors)
+            elif errors == "ignore":
+                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1:], errors)
+            else:
+                raise ValueError("unsupported error handling")
+            return outres.decode() + out1, inlen + len1 + 1
+
 def codec_factory(encoding):
     encoder = iconv.open(encoding, "utf-8")
     decoder = iconv.open("utf-8", encoding)
 
-    def encode(input, errors="strict", bufsize=None):
-        msg = input.encode()
+    def encode(inp, errors="strict"):
+        msg = inp.encode()
 
-        if bufsize is None:
-            bufsize = len(msg)
+        return _iconv_encode_impl(encoder, msg, errors)
 
-        try:
-            return encoder.iconv(msg, bufsize), len(msg)
-        except iconv.error as e:
-            errstring, code, inlen, outres = e.args
-            if code == errno.E2BIG:
-                # outbuffer was too small, increase size a bit and try to encode rest
-                out1, len1 = encode(msg[inlen :].decode(), errors, bufsize - inlen + max(bufsize // 10, 3))
-                return outres + out1, inlen + len1
-            if code == errno.EINVAL:
-                # An incomplete multibyte sequence has been
-                # encountered in the input. Should not happen in Unicode
-                raise AssertionError("EINVAL in encode")
-            if code == errno.EILSEQ:
-                # An invalid multibyte sequence has been encountered
-                # in the input. Used to indicate that the character is
-                # not supported in the target code
-                if errors == "strict":
-                    raise UnicodeError(*e.args)
-                if errors == "replace":
-                    out1, len1 = encode((u"?" + msg[inlen :].decode()[1:]), errors)
-                elif errors == "ignore":
-                    out1, len1 = encode(msg[inlen :].decode()[1:], errors)
-                else:
-                    raise ValueError("unsupported error handling")
-                return outres + out1, inlen + len1 + 1
-            raise
-
-    def decode(msg, errors="strict", bufsize=None):
-        if bufsize is None:
-            bufsize = len(msg)
-
-        try:
-            return decoder.iconv(msg, bufsize).decode(), len(msg)
-        except iconv.error as e:
-            errstring, code, inlen, outres = e.args
-            if code == errno.E2BIG:
-                # buffer too small
-                out1, len1 = decode(msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3))
-                return outres.decode() + out1, inlen + len1
-            if code == errno.EINVAL:
-                # An incomplete multibyte sequence has been
-                # encountered in the input.
-                return outres.decode(), inlen
-            if code == errno.EILSEQ:
-                # An invalid multibyte sequence has been encountered
-                # in the input. Ignoring or replacing it is hard to
-                # achieve, just try one character at a time
-                if errors == "strict":
-                    raise UnicodeError(*e.args)
-                if errors == "replace":
-                    outres += u"\uFFFD".encode()
-                    out1, len1 = decode(msg[inlen + 1:], errors)
-                elif errors == "ignore":
-                    out1, len1 = decode(msg[inlen + 1:], errors)
-                else:
-                    raise ValueError("unsupported error handling")
-                return outres.decode() + out1, inlen + len1 + 1
+    def decode(msg, errors="strict"):
+        return _iconv_decode_impl(decoder, msg, errors)
 
     return encode, decode
 
