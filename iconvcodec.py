017f359f0967c0846093e5b3746ca0b9b1ec83cc
Bodo Graumann <mail@bodograumann.de>
2021-06-05 14:56:34 +0200

    Apply code-formatting with black

--- iconvcodec.py
+++ iconvcodec.py
@@ -1,8 +1,9 @@
 import sys, iconv, codecs, errno
 
 
-_ENCODE_REPLACECHAR = '?'.encode()
-_DECODE_REPLACECHAR = u'\uFFFD'.encode()
+_ENCODE_REPLACECHAR = "?".encode()
+_DECODE_REPLACECHAR = u"\uFFFD".encode()
+
 
 def _iconv_encode_impl(encoder, msg, errors, bufsize=None):
     if bufsize is None:
@@ -14,7 +15,9 @@ def _iconv_encode_impl(encoder, msg, errors, bufsize=None):
         errstring, code, inlen, outres = e.args
         if code == errno.E2BIG:
             # outbuffer was too small, increase size a bit and try to encode rest
-            out1, len1 = _iconv_encode_impl(encoder, msg[inlen :], errors, bufsize - inlen + max(bufsize // 10, 3))
+            out1, len1 = _iconv_encode_impl(
+                encoder, msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3)
+            )
             return outres + out1, inlen + len1
         if code == errno.EINVAL:
             # An incomplete multibyte sequence has been
@@ -27,13 +30,20 @@ def _iconv_encode_impl(encoder, msg, errors, bufsize=None):
             if errors == "strict":
                 raise UnicodeError(*e.args)
             if errors == "replace":
-                out1, len1 = _iconv_encode_impl(encoder, (_ENCODE_REPLACECHAR + msg[inlen :].decode()[1:].encode()), errors)
+                out1, len1 = _iconv_encode_impl(
+                    encoder,
+                    (_ENCODE_REPLACECHAR + msg[inlen:].decode()[1:].encode()),
+                    errors,
+                )
             elif errors == "ignore":
-                out1, len1 = _iconv_encode_impl(encoder, msg[inlen :].decode()[1:].encode(), errors)
+                out1, len1 = _iconv_encode_impl(
+                    encoder, msg[inlen:].decode()[1:].encode(), errors
+                )
             else:
                 raise ValueError("unsupported error handling")
             return outres + out1, inlen + len1 + 1
         raise
+
 
 def _iconv_decode_impl(decoder, msg, errors, bufsize=None):
     if bufsize is None:
@@ -45,7 +55,9 @@ def _iconv_decode_impl(decoder, msg, errors, bufsize=None):
         errstring, code, inlen, outres = e.args
         if code == errno.E2BIG:
             # buffer too small
-            out1, len1 = _iconv_decode_impl(decoder, msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3))
+            out1, len1 = _iconv_decode_impl(
+                decoder, msg[inlen:], errors, bufsize - inlen + max(bufsize // 10, 3)
+            )
             return outres.decode() + out1, inlen + len1
         if code == errno.EINVAL:
             # An incomplete multibyte sequence has been
@@ -59,12 +71,13 @@ def _iconv_decode_impl(decoder, msg, errors, bufsize=None):
                 raise UnicodeError(*e.args)
             if errors == "replace":
                 outres += _DECODE_REPLACECHAR
-                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1:], errors)
+                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1 :], errors)
             elif errors == "ignore":
-                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1:], errors)
+                out1, len1 = _iconv_decode_impl(decoder, msg[inlen + 1 :], errors)
             else:
                 raise ValueError("unsupported error handling")
             return outres.decode() + out1, inlen + len1 + 1
+
 
 def codec_factory(encoding):
     encoder = iconv.open(encoding, "utf-8")
