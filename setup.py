003c03beae70eaa7085bb4bcbe4e6954c993d7bd
Stan Ulbrych <stan@ulbrych.org>
2025-11-01 11:09:10 +0000

    Add pre-commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 # On Linux, iconv is part of glibc and doesn't need separate linkage
 # Otherwise, link against libiconv
-if sys.platform.startswith('linux'):
+if sys.platform.startswith("linux"):
     libraries = []
 else:
     libraries = ["iconv"]
