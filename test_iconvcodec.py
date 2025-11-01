1f064a0618dee0cdf145d1c1f164ffe17b8a10b6
Stan Ulbrych <stan@ulbrych.org>
2025-11-01 21:45:14 +0000

    Lint

--- test_iconvcodec.py
+++ test_iconvcodec.py
@@ -5,7 +5,7 @@ import sys
 
 
 class TestIconvcodecModule(unittest.TestCase):
-    @unittest.skipUnless(sys.platform.startswith('linux'), "Linux only test")
+    @unittest.skipUnless(sys.platform.startswith("linux"), "Linux only test")
     def test_encode(self):
         bytestring = "Hallo".encode("T.61")
         self.assertEqual(bytestring, b"Hallo")
@@ -13,17 +13,17 @@ class TestIconvcodecModule(unittest.TestCase):
     def test_encode_with_long_out(self):
         """Edge case where output has more bytes than input as utf-8"""
         bytestring = "™".encode("ASCII//TRANSLIT")
-        if sys.platform.startswith('linux'):
+        if sys.platform.startswith("linux"):
             self.assertEqual(bytestring, b"(TM)")
         else:
             self.assertEqual(bytestring, b"TM")
 
-    @unittest.skipUnless(sys.platform.startswith('linux'), "Linux only test")
+    @unittest.skipUnless(sys.platform.startswith("linux"), "Linux only test")
     def test_decode(self):
         string = b"Hallo".decode("T.61")
         self.assertEqual(string, "Hallo")
 
-    @unittest.skipUnless(sys.platform.startswith('linux'), "Linux only test")
+    @unittest.skipUnless(sys.platform.startswith("linux"), "Linux only test")
     def test_transliterate(self):
         string = "abc ß α € àḃç"
         bytestring = string.encode("ASCII//TRANSLIT")
@@ -37,7 +37,7 @@ class TestIconvcodecModule(unittest.TestCase):
         self.assertEqual(first, b"Foo")
         self.assertEqual(second, b"bar")
 
-    @unittest.skipUnless(sys.platform.startswith('linux'), "Linux only test")
+    @unittest.skipUnless(sys.platform.startswith("linux"), "Linux only test")
     def test_incremental_decode(self):
         decoder = codecs.getincrementaldecoder("UCS2")()
         first = decoder.decode(b"\x41")
