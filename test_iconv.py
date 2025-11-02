41db504c00ded46674cdc6ffe6ed27f645e47d3b
Stan Ulbrych <stan@ulbrych.org>
2025-11-02 10:28:51 +0000

    Commit

--- test_iconv.py
+++ test_iconv.py
@@ -12,3 +12,9 @@ class TestIconvExtension(unittest.TestCase):
         s = iconv.open("utf-16le", "utf-8")
         r = s.iconv(b"Hallo", 11)
         self.assertEqual(r, "Hallo".encode("utf-16le"))
+
+    def test_incorrect_input_type(self):
+        s = iconv.open("latin1", "utf-8")
+        with self.assertRaisesRegex(TypeError,
+                                    "iconv expects byte object as first argument"):
+            r = s.iconv("Hallo")
